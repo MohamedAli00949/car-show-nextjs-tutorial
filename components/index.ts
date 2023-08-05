ea6bfb7561976d7d755d34bfd36b79d755fe0a31
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-08-06 00:07:14 +0300

    Create car catalogue

--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,17 @@
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import SearchBar from "./SearchBar";
+import CustomFilter from "./CustomFilter";
+import CustomButton from "./CustomButton";
+import SearchManufacturer from "./SearchManufacturer";
 
 export {
   Hero,
   Navbar,
   Footer,
+  SearchBar,
+  CustomFilter,
+  CustomButton,
+  SearchManufacturer,
 }
