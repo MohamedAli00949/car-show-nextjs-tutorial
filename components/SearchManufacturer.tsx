8594196ebfe2a90a95c5914ced01840deb0fd424
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-08-06 16:40:26 +0300

    Handle search bar inputs

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -26,7 +26,7 @@ const SearchManufacturer = ({
 
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
           <Combobox.Button className="absolute top-[14px]">
             <Image
