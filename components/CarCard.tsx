883f4bcdfd2add4e9e4cd8c3194eb70398a654cf
MOHAMED ALI FAWZI <76850143+MohamedAli00949@users.noreply.github.com>
2023-08-06 05:34:33 +0300

    Create Car details

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 import Image from "next/image";
 import { CarProps } from "@/types";
 import { calculateCarRent } from "@/utils";
-import { CustomButton } from ".";
+import { CustomButton, CarDetails } from ".";
 
 interface CarCardProps {
   car: CarProps;
@@ -73,6 +73,12 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
+
+      <CarDetails 
+        isOpen={isOpen}
+        closeModal={() => setIsOpen(false)}
+        car={car}
+      />
     </div>
   );
 };
